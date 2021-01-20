ba57c4b4ea282e8988a72c54a0af70a90acdb102
mazhuravlev <m.zhuravlev61@gmail.com>
2021-01-20 21:24:54 +0300

    add day09 solution

--- src/Day09.fs
+++ src/Day09.fs
@@ -1,4 +1,58 @@
 module Day09
 
+let isSum numbers n =
+    seq {
+        for x in numbers do
+            for y in numbers do
+                if x <> y && x + y = n then yield true
+    }
+    |> Seq.contains true
+
+let isSumArr (nArr: uint64 array) =
+    let numbers = nArr.[0..nArr.Length - 2]
+    let n = nArr.[nArr.Length - 1]
+    isSum numbers n
+
+let rec takeUntilSum s leftNs ns =
+    if s = 0UL then
+        ns
+    else if s < 0UL then
+        []
+    else
+        match leftNs with
+        | [] -> []
+        | x :: xs -> takeUntilSum (s - x) xs (x :: ns)
+
+let findContigousSums numbers n =
+    let rec fn numbers sums =
+        match numbers with
+        | x :: xs -> (takeUntilSum n xs []) :: (fn xs sums)
+        | _ -> []
+
+    fn numbers (takeUntilSum n numbers [])
+    |> List.filter (fun x -> x.Length > 0)
+
+let findInvalidNumber =
+    Seq.windowed 26
+    >> (Seq.find (isSumArr >> not))
+    >> Array.last
+
 let run (Filename file) =
-    "", ""
+    let data =
+        System.IO.File.ReadAllLines file
+        |> Seq.filter (System.String.IsNullOrWhiteSpace >> not)
+        |> Seq.map uint64
+
+    let getResult1 =
+        findInvalidNumber
+        >> sprintf "First number which is not sum of previous 25 numbers is %i"
+
+    let getResult2 =
+        findInvalidNumber 
+        >> findContigousSums (List.ofSeq data)
+        >> List.min
+        >> List.sort
+        >> (fun l -> l.Head + (l |> List.rev |> List.head))
+        >> sprintf "Sum of min and max of contigous sequence that sums to invalid number is %i"
+
+    getResult1 data, getResult2 data
--- src/Day09Tests.fs
+++ src/Day09Tests.fs
@@ -2,7 +2,41 @@ module Day09Tests
 
 open FsUnit.Xunit
 open Xunit
+open Day09
+
+let inList = [ 1UL; 2UL; 3UL; 4UL; 5UL ]
 
 type Day09Tests() =
-    let ``test`` =
-        Assert.Equal(true, true)
+    [<Fact>]
+    let ``Can test if number is sum of numbers from list`` () =
+        Assert.Equal(true, isSum inList 9UL)
+        Assert.Equal(true, isSum inList 3UL)
+
+    [<Fact>]
+    let ``Can test if number is not sum of numbers from list`` () =
+        Assert.Equal(false, isSum inList 20UL)
+        Assert.Equal(false, isSum inList 1UL)
+
+    [<Fact>]
+    let ``Can test if array satisfies isSum`` () =
+        Assert.Equal(true, isSumArr (inList @ [ 9UL ] |> Array.ofList))
+
+        Assert.Equal(true, isSumArr (inList @ [ 3UL ] |> Array.ofList))
+
+    [<Fact>]
+    let ``Can test if array not satisfies isSum`` () =
+        Assert.Equal(false, isSumArr (inList @ [ 20UL ] |> Array.ofList))
+
+        Assert.Equal(false, isSumArr (inList @ [ 1UL ] |> Array.ofList))
+
+    [<Fact>]
+    let ``Can find sum numbers`` () =
+        let actual = takeUntilSum 6UL inList []
+        let expected = [ 3UL; 2UL; 1UL ]
+        expected |> should equal actual
+
+    [<Fact>]
+    let ``Returns when sum numbers cant be found`` () =
+        let actual = takeUntilSum 20UL inList []
+        let expected = List.empty<uint64>
+        expected |> should equal actual
